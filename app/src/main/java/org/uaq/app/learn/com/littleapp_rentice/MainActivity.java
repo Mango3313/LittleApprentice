ddfea78fdfab944c2f0c8526391ab1c2c6304bdb
Mango3313 <daft_noob3313@live.com>
2018-06-01 23:21:10 -0500

    Permisos de microfono añadidos

--- app/src/main/java/org/uaq/app/learn/com/littleapp_rentice/MainActivity.java
+++ app/src/main/java/org/uaq/app/learn/com/littleapp_rentice/MainActivity.java
@@ -1,24 +1,42 @@
 package org.uaq.app.learn.com.littleapp_rentice;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int REQUEST_AUDIO = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Little App-rentice");
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.RECORD_AUDIO)){
+                Toast.makeText(this,"Permisos especificos",Toast.LENGTH_SHORT).show();
+            }else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},REQUEST_AUDIO);
+            }
+        }
         Fragment fragmentLog = new fragment_login();
         final FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
         transaction.replace(R.id.logcontainer,fragmentLog,"login");
         transaction.commit();
+
+
         //fm.
     }
 }
